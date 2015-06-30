84c6b72dadff2753c16dc7d9f7fc70b6f8a41967
Simon Cropp <simon.cropp@gmail.com>
2015-06-30 12:26:18 +1000

    init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# XsdGenerator
+Historically, inside the NServiceBus installer, we shipped a tool called XsdGenerator.exe. XsdGenerator is a command line tool that will generate an XSD file using an assembly of messages as input.  This is for the case where you don't want to directly distribute your messages assembly, but would rather have consumers build their own.
+
+The XsdGenerator.exe tool is now deprecated. The code for this tool has been moved to this repository. It will no longer be shipped as part of any NServiceBus tooling.
+
+If you still want to use XsdGenerator.exe feel free to use the code inside this repository to compile a copy.
+
+If, for any reason, the above options do not meet your specific requirements please don't hesitate to contacts about this decision.
---
+++ src/XsdGenerator/ExtensionMethods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NServiceBus.Serializers.XML.XsdGenerator
+{
+    using System.Collections.Concurrent;
+
+    static class ExtensionMethods
+    {
+        static ConcurrentDictionary<RuntimeTypeHandle, string> TypeToNameLookup = new ConcurrentDictionary<RuntimeTypeHandle, string>();
+        public static string SerializationFriendlyName(this Type t)
+        {
+            return TypeToNameLookup.GetOrAdd(t.TypeHandle, typeHandle =>
+            {
+                var index = t.Name.IndexOf('`');
+                if (index >= 0)
+                {
+                    var result = t.Name.Substring(0, index) + "Of";
+                    var args = t.GetGenericArguments();
+                    for (var i = 0; i < args.Length; i++)
+                    {
+                        result += args[i].SerializationFriendlyName();
+                        if (i != args.Length - 1)
+                        {
+                            result += "And";
+                        }
+                    }
+
+                    if (args.Length == 2)
+                    {
+                        if (typeof(KeyValuePair<,>).MakeGenericType(args[0], args[1]) == t)
+                        {
+                            result = "NServiceBus." + result;
+                        }
+                    }
+
+                    return result;
+                }
+                return Type.GetTypeFromHandle(typeHandle).Name;
+            });
+        }
+    }
+}
